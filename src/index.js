8295ef12eb2de052141160cd93ed8b47b65d9c29
Emil Zhou <emilzhou@thinkgeo.com>
2019-01-21 16:45:16 +0800

    Move LocationCateGories to ReverseGeocoding Folder

--- src/index.js
+++ src/index.js
@@ -2,16 +2,18 @@ import $Base$Client from "./Advanced/BaseClient";
 import $Elevation$Client from "./Elevation/ElevationClient";
 import $Projection$Client from "./Projection/ProjectionClient";
 import $Geocoding$Client from "./Geocoding/GeocodingClient";
-import $Maps$Client from "./MapsTile/MapsClient";
+
 import $ReverseGeocoding$Client from "./ReverseGeocoding/ReverseGeocodingClient";
+import $Location$Categories from "./ReverseGeocoding/LocationCategories";
+
 import $Color$Client from "./ColorUtilities/ColorClient";
 
+import $Maps$Client from "./MapsTile/MapsClient";
 import $Raster$Map$Type from "./MapsTile/RasterMapType";
 import $Map$Projection from "./MapsTile/MapProjection";
 import $Tile$Resolution from "./MapsTile/TileResolution";
 import $Tile$Size from "./MapsTile/TileSize";
 
-import $Location$Categories from "./shared/LocationCategories";
 
 let tg = {};
 tg.BaseClient = $Base$Client;
